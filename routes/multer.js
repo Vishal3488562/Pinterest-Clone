b8935685612260996d17288ac20a96b5fd14f347
Vishal Gupta <vishal3488562@gmail.com>
2024-03-13 11:27:39 +0530

    intial commit

--- routes/multer.js
+++ routes/multer.js
@@ -6,7 +6,7 @@ console.log("Hello")
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb){
-        cb(null, './public/images/uploads')
+        cb(null, './public/images')
     },
     filename: function (req, file, cb){
         const uniqueFilename = uuidv4();
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 
-
 const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
